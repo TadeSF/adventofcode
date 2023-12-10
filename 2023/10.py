de1eee6ba015cc2d829e53d3dd8fa629bcb6563b
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2023-12-10 12:58:05 +0100

    🧹 chore: Cleanup branch: main

---
+++ 2023/10.py
@@ -0,0 +1,13 @@
+from misc import start_program
+
+
+
+def main_1,
+    pass
+
+def main_2,
+    pass
+
+
+if __name__ == "__main__":
+    start_program([main_1, main_2])
--- 2023/misc.py
+++ 2023/misc.py
@@ -1,16 +1,27 @@
 # misc.py:
-import sys 
+import sys
 from typing import List, NoReturn
 
 
 def start_program(main_functions: List[NoReturn]):
-    if len(sys.argv) < 3:
-        print("Missing second argument (task_number): 1 or 2\npython3 8.py <input_file> <task_number>")
-
+    if len(sys.argv) < 2:
+        print(
+            "Missing second argument (task_number): 1 or 2\npython3 8.py <input_file> [<task_number>]"
+        )
+        return
+        
     with open(f"data/{sys.argv[1]}") as file:
         data = [line.replace("\n", "").strip() for line in file.readlines()]
 
-    for index, function in enumerate(main_functions):
-        if sys.argv[2] == str(index + 1):
+
+    if len(sys.argv) == 2:
+        print(f"Starting both tasks from {sys.argv[0]}")
+        for function in main_functions:
+            print(f"\n\n---\nRunning {function.__name__}:\n")
             function(data)
-            break
+
+    else:
+        for index, function in enumerate(main_functions):
+            if sys.argv[2] == str(index + 1):
+                function(data)
+                break
